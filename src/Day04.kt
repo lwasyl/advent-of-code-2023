700ec67c0c208982403d86e4132d893d18ddf867
Łukasz Wasylkowski <g7zm2717@duck.com>
2023-12-04 22:25:21 +0100

    Day 4

---
+++ src/Day04.kt
@@ -0,0 +1,63 @@
+import java.util.ArrayDeque
+import kotlin.math.pow
+
+fun main() {
+
+    fun parseCard(input: String): Card {
+        val (cardText, numberText) = input.split(':')
+        val (winningNumbers, ownNumbers) = numberText.split('|')
+
+        return Card(
+            number = cardText.substringAfter(' ').trimToInt(),
+            winningNumbers = winningNumbers.split(' ').mapNotNull { it.trimToIntOrNull() }.toSet(),
+            ownNumbers = ownNumbers.split(' ').mapNotNull { it.trimToIntOrNull() }.toSet(),
+        )
+    }
+
+    fun Card.score() = winningNumbers.intersect(ownNumbers).count()
+
+    fun part1(input: List<String>): Int {
+        return input
+            .asSequence()
+            .map(::parseCard)
+            .map(Card::score)
+            .filter { it >= 1 }
+            .map { 2.0.pow(it - 1.0).toInt() }
+            .sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        var score = 0
+        val cardNumbersToProcess = ArrayDeque(List(input.size) { it + 1 })
+        val scores = input.asSequence()
+            .map(::parseCard)
+            .associate { it.number to it.score() }
+
+        while (cardNumbersToProcess.isNotEmpty()) {
+            val number = cardNumbersToProcess.removeLast()
+            val cardScore = scores.getValue(number)
+            cardNumbersToProcess.addAll(List(cardScore) { number + 1 + it })
+
+            score += 1
+        }
+
+        return score
+    }
+
+    testAll(
+        day = 4,
+        part1Fn = ::part1,
+        part1TestSolution = 13,
+        part2Fn = ::part2,
+        part2TestSolution = 30,
+    )
+}
+
+data class Card(
+    val number: Int,
+    val winningNumbers: Set<Int>,
+    val ownNumbers: Set<Int>,
+)
+
+fun String.trimToInt() = trim().toInt()
+fun String.trimToIntOrNull() = trim().toIntOrNull()
--- src/Utils.kt
+++ src/Utils.kt
@@ -3,6 +3,9 @@ import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 import kotlin.reflect.KFunction1
+import kotlin.time.DurationUnit
+import kotlin.time.TimedValue
+import kotlin.time.measureTimedValue
 
 /**
  * Reads lines from the given input txt file.
@@ -21,6 +24,9 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  */
 fun Any?.println(prefix: String? = null) = kotlin.io.println("$prefix: $this")
 
+fun TimedValue<Any>.println(prefix: String? = null) =
+    kotlin.io.println("$prefix: $value [${duration.toString(unit = DurationUnit.MILLISECONDS, decimals = 4)}]")
+
 internal fun testAll(
     day: Int,
     part1Fn: KFunction1<List<String>, Any>,
@@ -30,30 +36,26 @@ internal fun testAll(
 ) {
     val dayString = "Day${String.format("%02d", day)}"
     readInput("${dayString}_test")
-        .let(part1Fn)
+        .let { measureTimedValue { part1Fn(it) } }
         .let {
             it.println("Part 1 test")
-            check(it == part1TestSolution)
+            check(it.value == part1TestSolution)
         }
 
     readInput(dayString)
-        .let(part1Fn)
-        .let {
-            it.println("Part 1 output")
-        }
+        .let { measureTimedValue { part1Fn(it) } }
+        .println("Part 1 output")
 
     if (part2TestSolution == null) return
 
     readInput("${dayString}_2_test")
-        .let(part2Fn)
+        .let { measureTimedValue { part2Fn(it) } }
         .let {
             it.println("Part 2 test")
-            check(it == part2TestSolution)
+            check(it.value == part2TestSolution)
         }
 
     readInput(dayString)
-        .let(part2Fn)
-        .let {
-            it.println("Part 2 output")
-        }
+        .let { measureTimedValue { part2Fn(it) } }
+        .println("Part 2 output")
 }
