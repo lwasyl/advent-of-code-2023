7b615b09e92e86f40cbaf2c3239919d333a0f990
Łukasz Wasylkowski <g7zm2717@duck.com>
2023-12-12 23:13:02 +0100

    Simple hacky generic memoization

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,3 +1,5 @@
+import kotlin.properties.Delegates
+
 fun main() {
 
     fun parseInput(input: List<String>): Day12Input {
@@ -13,14 +15,18 @@ fun main() {
         )
     }
 
-    fun part1(input: List<String>): Long {
+        fun part1(input: List<String>): Long {
+        val memoizedSolution = memoized(::day12CountSolutions)
+
         return parseInput(input).springs
             .asSequence()
-            .map { day12CountSolutions(it.states, it.groups) }
+            .map { memoizedSolution(it.states, it.groups) }
             .sum()
     }
 
     fun part2(input: List<String>): Long {
+        val memoizedSolution = memoized(::day12CountSolutions)
+
         return parseInput(input)
             .springs
             .asSequence()
@@ -30,7 +36,7 @@ fun main() {
                     groups = List(5) { row.groups }.flatten(),
                 )
             }
-            .map { day12CountSolutions(it.states, it.groups) }
+            .map { memoizedSolution(it.states, it.groups) }
             .sum()
     }
 
@@ -43,32 +49,28 @@ fun main() {
     )
 }
 
-private val day12Cache = mutableMapOf<Pair<String, List<Int>>, Long>()
-private fun day12CountSolutions(input: String, groups: List<Int>): Long {
-    day12Cache[input to groups]?.let { return it }
-    fun Long.alsoCache() = this.also { day12Cache[input to groups] = it }
-
+private fun day12CountSolutions(input: String, groups: List<Int>, recurse: (String, List<Int>) -> Long): Long {
     when {
-        input.isBlank() && groups.isEmpty() -> return 1L.alsoCache()
-        input.isBlank() && groups.isNotEmpty() -> return 0L.alsoCache()
-        input.isNotEmpty() && groups.isEmpty() -> return (if (input.any { it == '#' }) 0L else 1L).alsoCache()
-        input.length < groups.first() -> return 0L.alsoCache()
-        input.length < groups.sum() + groups.size - 1 -> return 0L.alsoCache()
+        input.isBlank() && groups.isEmpty() -> return 1L
+        input.isBlank() && groups.isNotEmpty() -> return 0L
+        input.isNotEmpty() && groups.isEmpty() -> return (if (input.any { it == '#' }) 0L else 1L)
+        input.length < groups.first() -> return 0L
+        input.length < groups.sum() + groups.size - 1 -> return 0L
     }
 
     return when (input.first()) {
         '#' -> {
             val neededInRow = groups.first()
 
-            (0 until neededInRow).forEach { if (input[it] == '.') return 0L.alsoCache() }
-            if (input.getOrNull(neededInRow) == '#') return 0L.alsoCache()
+            (0 until neededInRow).forEach { if (input[it] == '.') return 0L }
+            if (input.getOrNull(neededInRow) == '#') return 0L
 
-            day12CountSolutions(input.drop(neededInRow + 1), groups.drop(1))
+            recurse(input.drop(neededInRow + 1), groups.drop(1))
         }
-        '.' -> day12CountSolutions(input.drop(1), groups)
-        '?' -> day12CountSolutions("#${input.drop(1)}", groups) + day12CountSolutions(".${input.drop(1)}", groups)
+        '.' -> recurse(input.drop(1), groups)
+        '?' -> recurse("#${input.drop(1)}", groups) + recurse(".${input.drop(1)}", groups)
         else -> error("unknown input")
-    }.alsoCache()
+    }
 }
 
 private data class Day12Input(
@@ -79,3 +81,12 @@ private data class SpringRow(
     val states: String,
     val groups: List<Int>,
 )
+
+fun <P1, P2, R> memoized(fn: (P1, P2, (P1, P2) -> R) -> R): ((P1, P2) -> R) {
+    val cache = mutableMapOf<Pair<P1, P2>, R>()
+    var recursiveCall: ((P1, P2) -> R) by Delegates.notNull()
+    recursiveCall = { p1, p2 ->
+        cache.getOrPut(p1 to p2) { fn(p1, p2, recursiveCall) }
+    }
+    return recursiveCall
+}
